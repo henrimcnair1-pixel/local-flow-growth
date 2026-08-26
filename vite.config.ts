1b87d8fa130b71d42ee9b07c48c85815e415e258
Local Flow Growth <you@example.com>
2026-08-26 12:03:25 +0100

    Fix asset paths for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command }) => {
-  const isBuild = command === 'build';
-
+export default defineConfig(() => {
   return {
-    base: isBuild ? '/local-flow-growth/' : '/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
